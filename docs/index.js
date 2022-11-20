431d6af7c5c19f5df2b1431a9dc788531983f183
Mactavsin <69454747+Mactavsin@users.noreply.github.com>
2022-11-20 18:08:37 +0300

    Retranspile index.js

--- docs/index.js
+++ docs/index.js
@@ -17,7 +17,7 @@ Difference_ListCE[1].id = "difference-list-1";
 document.body.appendChild(Difference_ListCE[0]);
 document.body.appendChild(Difference_ListCE[1]);
 const Current_Location = window.location;
-const Search_Parameters = new URLSearchParams("?locale=tr-TR&differenceType=stopData&includeUnchangedData=false&commit0=384ff19e1b17656bf24f1d5459d4b8ae84877fc2&commit1=a7ffb6dcfa3f7f72a9feee48f187864cebae5086" /* "?locale=tr-TR&differenceType=routeData&includeUnchangedData=false&commit0=5cc805d0c12857e9e2cd2bfc293f71bcb403ff6c&commit1=4df5735127d4e3c4a4b5fb3b782fd9ee0d94d4cf" */ /* Current_Location.search */);
+const Search_Parameters = new URLSearchParams(Current_Location.search);
 function RenderWebpage() {
     return __awaiter(this, void 0, void 0, function* () {
         const Locale_Name = Search_Parameters.get("locale");
